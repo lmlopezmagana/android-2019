02609fc0100593e7c9f414f2f95f474bcaa84d47
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2019-01-18 12:41:16 +0100

    Ejemplo sencillo de Retrofit con un ListActivity

---
+++ 03_Retrofit/app/src/main/java/android/salesianostriana/com/a03_retrofit/GitHubService.java
@@ -0,0 +1,15 @@
+package android.salesianostriana.com.a03_retrofit;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface GitHubService {
+
+    @GET("users/{user}/repos")
+    Call<List<Repo>> listRepos(@Path("user") String user);
+
+
+}
--- 03_Retrofit/app/src/main/java/android/salesianostriana/com/a03_retrofit/MainActivity.java
+++ 03_Retrofit/app/src/main/java/android/salesianostriana/com/a03_retrofit/MainActivity.java
@@ -1,14 +1,25 @@
 package android.salesianostriana.com.a03_retrofit;
 
 import android.app.ListActivity;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class MainActivity extends ListActivity {
+
+    GitHubService service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,51 @@ public class MainActivity extends ListActivity {
 
         setListAdapter(adapter);
 
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        service = retrofit.create(GitHubService.class);
+
+        new LoadDataTask().execute();
+
+
+    }
+
+    public void setAdapter(ListAdapter adapter) {
+        setListAdapter(adapter);
+    }
+
+    private class LoadDataTask extends AsyncTask<Void, Void, List<Repo>> {
+
+
+
+        @Override
+        protected List<Repo> doInBackground(Void... voids) {
+            Call<List<Repo>> repos = service.listRepos("octocat");
+            Response<List<Repo>> response = null;
+            try {
+               response = repos.execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (response != null) {
+                return response.body();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(List<Repo> repoList) {
+            setAdapter(new ArrayAdapter<Repo>(
+                    MainActivity.this,
+                    android.R.layout.simple_list_item_1,
+                    repoList));
+        }
 
     }
 }
